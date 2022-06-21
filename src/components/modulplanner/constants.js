ea0a2697e483d836fb5b626843be7f5314548cd3
laniw <lani.julian.wagner+github@gmail.com>
2022-06-21 22:46:32 +0200

    Adds jest tests for util class

--- src/components/modulplanner/constants.js
+++ src/components/modulplanner/constants.js
@@ -1,4 +1,5 @@
 export const LOCAL_STORAGE_USER_DATA_KEY = "studenthub_userData";
+// TODO(#3)
 
 export const Status = {
   NOT_TAKEN: 0,
---
+++ src/test/util.test.js
@@ -0,0 +1,19 @@
+import {downloadableFileName} from "../util";
+
+test("downloadableFileName leadGiven leadInFilename", () => {
+  const lead = "testing";
+  const src = "testFunc";
+  const date = new Date("2022-06-21T22:04:31");
+  expect(downloadableFileName(lead, src, date)).toBe(
+    "testing_testFunc_2022-06-21_22-04-31.json"
+  );
+});
+
+test("downloadableFileName leadMissing leadMissing", () => {
+  const lead = "testing";
+  const src = "testFunc";
+  const date = new Date("2022-06-21T22:04:31");
+  expect(downloadableFileName("", src, date)).toBe(
+    "testFunc_2022-06-21_22-04-31.json"
+  );
+});
