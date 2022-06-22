f360c24d1eb92882668dd289a75e7eef6b17e016
laniw <lani.julian.wagner+github@gmail.com>
2022-06-22 15:43:19 +0200

    Fits all modules into as little space as possible on desktop and mobile

--- src/components/modulplanner/ModuleChipCollection.svelte
+++ src/components/modulplanner/ModuleChipCollection.svelte
@@ -6,16 +6,17 @@
   export let userDataVal;
   export let source = ModuleGroupSource.UNKNOWN;
 
-  const threeCols =
-    source === ModuleGroupSource.ADDITIONAL_MODULES ||
-    source === ModuleGroupSource.PROJECTS;
-  const fourCols = source === ModuleGroupSource.CONTEXT;
+  const additionalModules = source === ModuleGroupSource.ADDITIONAL_MODULES;
+  const projects = source === ModuleGroupSource.PROJECTS;
+  const contextModules = source === ModuleGroupSource.CONTEXT;
 </script>
 
 <div
   class="grid lg:grid-cols-1 gap-3"
-  class:grid-cols-3={threeCols}
-  class:grid-cols-4={fourCols}>
+  class:grid-cols-4={additionalModules || projects || contextModules}
+  class:lg:grid-cols-12={additionalModules}
+  class:lg:grid-cols-1={projects}
+  class:lg:grid-cols-2={contextModules}>
   {#each modules as module}
     <ModuleChip {module} {userDataVal} />
   {/each}
