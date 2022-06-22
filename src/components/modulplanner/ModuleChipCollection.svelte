86e146c31ef79efd632fed72249ab59c96d82a1e
laniw <lani.julian.wagner+github@gmail.com>
2022-06-22 14:59:36 +0200

    Displays any context modules in four cols on mobile

--- src/components/modulplanner/ModuleChipCollection.svelte
+++ src/components/modulplanner/ModuleChipCollection.svelte
@@ -5,12 +5,17 @@
   export let modules;
   export let userDataVal;
   export let source = ModuleGroupSource.UNKNOWN;
+
+  const threeCols =
+    source === ModuleGroupSource.ADDITIONAL_MODULES ||
+    source === ModuleGroupSource.PROJECTS;
+  const fourCols = source === ModuleGroupSource.CONTEXT;
 </script>
 
 <div
   class="grid lg:grid-cols-1 gap-2 lg:gap-3"
-  class:grid-cols-3={source === ModuleGroupSource.ADDITIONAL_MODULES ||
-    source === ModuleGroupSource.PROJECTS || source === ModuleGroupSource.CONTEXT}>
+  class:grid-cols-3={threeCols}
+  class:grid-cols-4={fourCols}>
   {#each modules as module}
     <ModuleChip {module} {userDataVal} />
   {/each}
