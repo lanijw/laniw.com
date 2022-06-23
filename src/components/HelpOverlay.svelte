a9de57d8483e1fac9d842d3d859b2ad19e5f07b7
laniw <lani.julian.wagner+github@gmail.com>
2022-06-24 00:04:52 +0200

    Implements collaborator credit in sidebar
    Fixes #22

--- src/components/HelpOverlay.svelte
+++ src/components/HelpOverlay.svelte
@@ -1,5 +1,27 @@
 <script>
   import HelpIcon from "./icons/HelpIcon.svelte";
+  import {onMount} from "svelte";
+
+  onMount(() => {
+    getCollaborators();
+  });
+
+  let collaborators = [];
+  async function getCollaborators() {
+    const apiUrl =
+      "https://api.github.com/repos/lanijw/laniw.com/pulls?state=all";
+    collaborators = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => [
+        ...new Set(data.map(r => r.user.login).filter(n => n !== "lanijw"))
+      ]);
+    console.log(collaborators);
+  }
 </script>
 
 <div class="d-drawer d-drawer-end">
@@ -42,6 +64,19 @@
         there aren't any issues just create an issue and I'm sure I can find you
         something to do.
       </p>
+      {#if collaborators.length}
+        <h3 class="mt-4 text-2xl">Collaborators</h3>
+        <p>
+          Many thanks to the people who helped me work on my projects on
+          <a href="https://laniw.com">laniw.com</a>!
+        </p>
+        <div class="flex flex-row flex-wrap pt-2">
+          {#each collaborators as collaborator}
+            <div class="p-1 rounded bg-primary text-primary-content"
+              >{collaborator}</div>
+          {/each}
+        </div>
+      {/if}
     </div>
   </div>
 </div>
