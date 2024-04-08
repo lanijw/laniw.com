1972c48de35fe3e4950ec547ed4d9d6b768c381a
laniw <lani.julian.wagner+github@gmail.com>
2024-04-08 10:41:46 +0200

    Styles main and logo container for home page.

--- web-components/content/root.css.js
+++ web-components/content/root.css.js
@@ -1,5 +1,15 @@
 const css = `
+<style>
+.main-container {
+  width: clamp(22rem, 50%, 64rem);
+  margin: 0 auto;
+}
 
+.logo-container {
+  width: 20rem;
+  margin: 0 auto;
+}
+</style>
 `;
 
 export default css;
